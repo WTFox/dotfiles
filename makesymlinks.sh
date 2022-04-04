e54a17c765ca205396d40c6a3d89ef280d8c53b3
A. Fox <anthonyfox1988@gmail.com>
2022-04-03 19:41:40 -0700

    tidy up makesymlinks

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -17,21 +17,12 @@ files="bashrc zshrc p10k.zsh psqlrc gitaliases gitignore gitconfig tmux.conf tmu
 
 ##########
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir || exit
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
     ln -sf $dir/$file ~/.$file
 done
 
