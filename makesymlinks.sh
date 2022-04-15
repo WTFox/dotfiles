a553ed40325335014d47f8504d41bdc9c2accdf3
A. Fox <anthonyfox1988@gmail.com>
2022-04-15 14:44:30 -0700

    Fri Apr 15 14:44:30 PDT 2022

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -13,7 +13,7 @@ dir=~/dotfiles
 olddir=~/dotfiles_old
 
 # list of files/folders to symlink in homedir
-files="bashrc zshrc p10k.zsh psqlrc gitaliases gitignore gitconfig tmux.conf tmux.conf.local vscode.vim"
+files="bashrc zshrc p10k.zsh psqlrc gitaliases gitignore gitconfig tmux.conf tmux.conf.local vscode.vim prettierrc"
 
 ##########
 
