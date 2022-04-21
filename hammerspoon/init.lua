67f14eae97224e02807cbc47a7f5b725e238d805
A. Fox <anthonyfox1988@gmail.com>
2022-04-21 07:58:29 -0700

    change hyper key

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,7 +9,7 @@ local function open_app(name)
 end
 
 --- quick open applications
-hs.hotkey.bind({'alt', 'shift'}, "Z", open_app("Visual Studio Code"))
-hs.hotkey.bind({'alt', 'shift'}, "X", open_app("iTerm"))
-hs.hotkey.bind({'alt', 'shift'}, "C", open_app("Brave Browser"))
-hs.hotkey.bind({'alt', 'shift'}, "M", open_app("Google Meet"))
+hs.hotkey.bind({ 'ctrl', 'shift' }, "Z", open_app("Visual Studio Code"))
+hs.hotkey.bind({ 'ctrl', 'shift' }, "X", open_app("iTerm"))
+hs.hotkey.bind({ 'ctrl', 'shift' }, "C", open_app("Brave Browser"))
+hs.hotkey.bind({ 'ctrl', 'shift' }, "M", open_app("Google Meet"))
