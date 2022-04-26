52f2520837fb415dc5baa0483b20339810a1ccf8
A. Fox <anthonyfox1988@gmail.com>
2022-04-26 11:13:31 -0700

    Add GoogleMeet hotkeys

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,3 +13,22 @@ hs.hotkey.bind({ 'ctrl', 'shift' }, "Z", open_app("Visual Studio Code"))
 hs.hotkey.bind({ 'ctrl', 'shift' }, "X", open_app("iTerm"))
 hs.hotkey.bind({ 'ctrl', 'shift' }, "C", open_app("Brave Browser"))
 hs.hotkey.bind({ 'ctrl', 'shift' }, "M", open_app("Google Meet"))
+
+-- Google Meet
+local function FocusGoogleMeet()
+    hs.application.launchOrFocus('Google Meet')
+end
+
+-- home key is global mute
+hs.hotkey.bind({}, 'home', function()
+    FocusGoogleMeet()
+    hs.eventtap.keyStroke({ "cmd" }, "d", 200)
+end
+)
+
+-- page up key is global enable/disable video
+hs.hotkey.bind({}, 'pageup', function()
+    FocusGoogleMeet()
+    hs.eventtap.keyStroke({ "cmd" }, "e", 200)
+end
+)
