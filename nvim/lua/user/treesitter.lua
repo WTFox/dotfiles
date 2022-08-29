328f1079473cb17332b939e25ae4fa809bfa4611
A. Fox <anthonyfox1988@gmail.com>
2022-08-29 09:18:18 -0700

    neovim setup complete

---
+++ nvim/lua/user/treesitter.lua
@@ -0,0 +1,11 @@
+local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+if not status_ok then
+	return
+end
+
+configs.setup({
+	ensure_installed = "all", -- one of "all" or a list of languages
+	autopairs = {
+		enable = true,
+	},
+})
