9e46ae3f045ed6690f6a577a064cf105abde54af
A. Fox <anthonyfox1988@gmail.com>
2022-09-14 20:09:00 -0700

    enable copilot for react

--- nvim/.config/nvim/lua/user/copilot.lua
+++ nvim/.config/nvim/lua/user/copilot.lua
@@ -7,6 +7,7 @@ vim.g.copilot_filetypes = {
   ["*"] = false,
   ["javascript"] = true,
   ["typescript"] = true,
+  ["typescriptreact"] = true,
   ["lua"] = false,
   ["rust"] = true,
   ["c"] = true,
