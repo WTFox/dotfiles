416c7309c0c6ac9c3c45b6e441de37417bbc79e1
A. Fox <anthonyfox1988@gmail.com>
2022-10-21 14:39:39 -0700

    Add refactoring.nvim

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -66,6 +66,7 @@ return packer.startup(function(use)
   }
   use { "xiyaowong/nvim-transparent" }
   use { "ThePrimeagen/harpoon" }
+  use { "ThePrimeagen/refactoring.nvim" }
 
   -- Colorschemes
   use { "folke/tokyonight.nvim" }
