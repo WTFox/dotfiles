5850be9d9b274d1a00d61a91df38d4bcf75976b7
A. Fox <anthonyfox1988@gmail.com>
2022-10-23 14:55:04 -0700

    dashboard enhancements

--- nvim/.config/nvim/lua/user/alpha.lua
+++ nvim/.config/nvim/lua/user/alpha.lua
@@ -27,6 +27,7 @@ dashboard.section.header.val = {
   [[⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠁⠁⠉⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠁⠉⠉⠈⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠁⠁⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀]],
 }
 
+dashboard.section.buttons.opts.spacing = 0
 dashboard.section.buttons.val = {
   dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
@@ -36,21 +37,18 @@ dashboard.section.buttons.val = {
   dashboard.button("r", "  Reload Config", ":source $MYVIMRC<CR>"),
   dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
-dashboard.section.buttons.opts.spacing = 0
 
 dashboard.section.footer.val = fortune()
-
 
 dashboard.config.layout = {
   dashboard.section.header,
   { type = "padding", val = 2 },
-  { type = "text", val = "neovim", opts = { position = "center" } },
+  { type = "text", val = "neovim", opts = { hl = "Number", position = "center" } },
   { type = "text", val = info_string(), opts = { position = "center" } },
   { type = "padding", val = 2 },
   dashboard.section.buttons,
   { type = "padding", val = 2 },
   dashboard.section.footer,
 }
-
 
 alpha.setup(dashboard.config)
