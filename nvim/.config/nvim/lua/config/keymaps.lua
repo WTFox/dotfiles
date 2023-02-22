37eeca1df5eaf879d15d0ed81c2e5d14cd453786
A. Fox <anthonyfox1988@gmail.com>
2023-02-22 15:29:57 -0800

    keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,20 @@
 -- keymaps are automatically loaded on the verylazy event
 -- default keymaps that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/keymaps.lua
 -- add any additional keymaps here
+
+-- open last search in telescope
 vim.keymap.set(
   "n",
   "<leader>sx",
   require("telescope.builtin").resume,
   { noremap = true, silent = true, desc = "resume" }
 )
+
+-- toggle dashboard (alpha)
+vim.keymap.set("n", "<leader>a", ":Alpha<cr>", { noremap = true, silent = true, desc = "dashboard" })
+
+-- next quickfix item
+vim.keymap.set("n", "<leader>]", ":cnext<cr>zz", { noremap = true, silent = true, desc = "next quickfix" })
+
+-- prev quickfix item
+vim.keymap.set("n", "<leader>[", ":cprev<cr>zz", { noremap = true, silent = true, desc = "prev quickfix" })
--- nvim/.config/nvim/lua/plugins/nvim-test.lua
+++ nvim/.config/nvim/lua/plugins/nvim-test.lua
@@ -1,3 +1,13 @@
+local wk = require("which-key")
+
+wk.register({
+  t = {
+    name = "test",
+    t = { "<cmd>TestNearest<cr>", "test nearest" },
+    f = { "<cmd>TestFile<cr>", "test file" },
+  },
+}, { prefix = "<leader>" })
+
 return {
   "klen/nvim-test",
   config = function()
