039ca4a5c71d114786ee31270e0262a4a8557d14
A. Fox <anthonyfox1988@gmail.com>
2023-03-02 16:29:44 -0800

    add autocommands to set/unset relative numbers

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set relative line numbers in normal mode and absolute line numbers in insert mode
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+  command = "set norelativenumber",
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  command = "set relativenumber",
+})
