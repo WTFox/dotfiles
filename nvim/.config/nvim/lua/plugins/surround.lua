bb4526193d97c741a65905c1272ce8c1f7ae8698
A. Fox <anthonyfox1988@gmail.com>
2023-03-16 13:27:13 -0700

    fix surround

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,14 +1,34 @@
+-- return {
+--   "echasnovski/mini.surround",
+--   opts = {
+--     mappings = {
+--       add = "gsa",
+--       delete = "gsd",
+--       find = "gsf",
+--       find_left = "gsF",
+--       highlight = "gsh",
+--       replace = "gsr",
+--       update_n_lines = "gsn",
+--     },
+--   },
+-- }
+
 return {
   "echasnovski/mini.surround",
+  event = "VeryLazy",
   opts = {
     mappings = {
-      add = "gsa",
-      delete = "gsd",
-      find = "gsf",
-      find_left = "gsF",
-      highlight = "gsh",
-      replace = "gsr",
-      update_n_lines = "gsn",
+      replace = "cs",
+      add = "ys",
+      delete = "ds",
+      highlight = "vs",
+      find = "yf",
+      find_left = "yF",
+      update_n_lines = "",
     },
+    search_method = "cover_or_next",
   },
+  config = function(_, opts)
+    require("mini.surround").setup(opts)
+  end,
 }
