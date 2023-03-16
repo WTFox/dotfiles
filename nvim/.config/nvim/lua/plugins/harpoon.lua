1f0afcaccc3224d31b659d3b25d5fe7b7b533128
A. Fox <anthonyfox1988@gmail.com>
2023-03-15 20:40:34 -0700

    add harpoon, update surround settings

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,15 @@
+return {
+  "ThePrimeagen/harpoon",
+  config = function()
+    require("harpoon").setup()
+    require("telescope").load_extension("harpoon")
+  end,
+  keys = {
+    { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon" },
+    { "<leader>ha", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Harpoon" },
+    { "<leader>ht", "<cmd>Telescope harpoon marks<cr>", desc = "Harpoon" },
+    { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Goto File 1" },
+    { "<leader>h2", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Goto File 2" },
+    { "<leader>h1", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Goto File 1" },
+  },
+}
