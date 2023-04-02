70f2e95d71f7f518f88c70cb0887d8a11bb4a1cc
A. Fox <anthonyfox1988@gmail.com>
2023-04-02 04:41:46 +0000

    colorscheme, rust

--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -4,4 +4,14 @@ return {
     { "neovim/nvim-lspconfig" },
     { "mfussenegger/nvim-dap" },
   },
+  opts = {
+    tools = {
+      inlay_hints = {
+        auto = true,
+        show_parameter_hints = true,
+        parameter_hints_prefix = "<-",
+        other_hints_prefix = "=>",
+      },
+    },
+  },
 }
