20e1c6f38d1e875d266fd09089fbf63b700eb9da
A. Fox <anthonyfox1988@gmail.com>
2023-04-21 09:08:33 -0700

    changes from the week

--- nvim/.config/nvim/lua/plugins/git-blame.lua
+++ nvim/.config/nvim/lua/plugins/git-blame.lua
@@ -3,4 +3,9 @@ vim.g.gitblame_date_format = "%Y-%m-%d %H:%M:%S"
 
 return {
   "f-person/git-blame.nvim",
+  event = "BufRead",
+  keys = {
+    { "<leader>goc", "<cmd>GitBlameOpenCommitURL<CR>", desc = "Open commit url" },
+    { "<leader>gof", "<cmd>GitBlameOpenFileURL<CR>", desc = "Open file url" },
+  },
 }
