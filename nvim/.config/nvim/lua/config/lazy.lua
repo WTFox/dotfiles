36231208bed36fb996bbcbb10b8fc73d6fc554ff
A. Fox <anthonyfox1988@gmail.com>
2023-05-23 13:57:51 -0700

    use neotestconfig from lazyvim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -12,10 +12,12 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.util.project" },
--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,53 +1,24 @@
 return {
   "nvim-neotest/neotest",
-  enabled = true,
   dependencies = {
+    "nvim-neotest/neotest-go",
     "nvim-neotest/neotest-python",
     "haydenmeade/neotest-jest",
     "rouge8/neotest-rust",
-    "nvim-neotest/neotest-go",
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "antoinemadec/FixCursorHold.nvim",
   },
-  config = function()
-    ---@ neotest.Config
-    require("neotest").setup({
-      adapters = {
-        require("neotest-python"),
-        require("neotest-jest"),
-        require("neotest-rust"),
-        require("neotest-go"),
-      },
-      quickfix = {
-        enabled = false,
-      },
-      output = {
-        enabled = true,
-        open_on_run = true,
-      },
-      output_panel = {
-        enabled = true,
-        open = "botright split | resize 15",
-      },
+  opts = function(_, opts)
+    opts.adapters = vim.list_extend(opts.adapters or {}, {
+      require("neotest-go")({
+        args = { "-tags=integration" },
+      }),
+      require("neotest-python")({
+        args = { "-m", "pytest" },
+      }),
+      require("neotest-jest")({}),
+      require("neotest-rust")({
+        args = { "--no-capture" },
+        dap_adapter = "lldb",
+      }),
     })
   end,
-  keys = {
-    { "<leader>tt", "<cmd>lua require('neotest').run.run()<cr>", desc = "Test nearest" },
-    { "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "Test file" },
-    { "<leader>tl", "<cmd>lua require('neotest').run.run_last({strategy = 'dap'})<cr>", desc = "Debug Last" },
-    { "<leader>td", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", desc = "Debug Nearest" },
-    { "<leader>to", "<cmd>lua require('neotest').output.open({ enter = true })<cr>", desc = "Output" },
-    {
-      "<leader>ts",
-      function()
-        require("neotest").summary.toggle()
-        local win = vim.fn.bufwinid("Neotest Summary")
-        if win > -1 then
-          vim.api.nvim_set_current_win(win)
-        end
-      end,
-      desc = "Summary",
-    },
-  },
 }
