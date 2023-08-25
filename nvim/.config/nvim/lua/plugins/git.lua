8f69c5d7fa27defa6f4e87a6c33840f088e2038d
A. Fox <anthonyfox1988@gmail.com>
2023-08-25 09:25:40 -0700

    add utility function to get main branch

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,3 +1,19 @@
+_G.get_main_branch = function()
+  local handle = io.popen("git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@'")
+  if not handle then
+    return nil
+  end
+
+  local result = handle:read("*a")
+  handle:close()
+
+  if not result or result == "" then
+    return nil
+  end
+
+  return result:gsub("\n", "")
+end
+
 return {
   {
     "tpope/vim-fugitive",
@@ -5,19 +21,12 @@ return {
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "blame" },
     },
   },
-  -- {
-  --   "FabijanZulj/blame.nvim",
-  --   keys = {
-  --     { "<leader>gb", "<cmd>ToggleBlame<cr>", desc = "blame" },
-  --   },
-  -- },
   {
     "sindrets/diffview.nvim",
     dependencies = "nvim-lua/plenary.nvim",
     keys = {
-      { "<leader>gdd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" },
-      { "<leader>gdm", "<cmd>DiffviewOpen main<cr>", desc = "DiffView (main)" },
-      { "<leader>gdM", "<cmd>DiffviewOpen master<cr>", desc = "DiffView (master)" },
+      { "<leader>gdd", "<cmd>DiffviewOpen<CR>", desc = "DiffView" },
+      { "<leader>gdm", "<cmd>DiffviewOpen " .. _G.get_main_branch() .. "<CR>", desc = "DiffView (main)" },
     },
   },
 }
