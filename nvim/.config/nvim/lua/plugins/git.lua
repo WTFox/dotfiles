4c751e4ecaf432aedee1941df64eed10dc08b859
A. Fox <anthonyfox1988@gmail.com>
2023-08-26 02:06:59 +0000

    updates and fix git branch check from erroring when not in a git rpeo

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,14 +1,14 @@
 _G.get_main_branch = function()
   local handle = io.popen("git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@'")
   if not handle then
-    return nil
+    return ""
   end
 
   local result = handle:read("*a")
   handle:close()
 
   if not result or result == "" then
-    return nil
+    return ""
   end
 
   return result:gsub("\n", "")
