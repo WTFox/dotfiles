40080f1340ba9157c6f737e0f08f8dc420438050
A. Fox <anthonyfox1988@gmail.com>
2023-08-26 03:29:22 +0000

    don't use global function

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,4 +1,4 @@
-_G.get_main_branch = function()
+local get_main_branch = function()
   local handle = io.popen("git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@'")
   if not handle then
     return ""
@@ -14,6 +14,7 @@ _G.get_main_branch = function()
   return result:gsub("\n", "")
 end
 
+local main_branch = get_main_branch()
 return {
   {
     "tpope/vim-fugitive",
@@ -26,7 +27,7 @@ return {
     dependencies = "nvim-lua/plenary.nvim",
     keys = {
       { "<leader>gdd", "<cmd>DiffviewOpen<CR>", desc = "DiffView" },
-      { "<leader>gdm", "<cmd>DiffviewOpen " .. _G.get_main_branch() .. "<CR>", desc = "DiffView (main)" },
+      { "<leader>gdm", "<cmd>DiffviewOpen " .. main_branch .. "<CR>", desc = "DiffView (" .. main_branch .. ")" },
     },
   },
 }
