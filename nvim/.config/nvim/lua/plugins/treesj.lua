6ea599565747d2ea508bc2d862ce553dce1091c8
A. Fox <anthonyfox1988@gmail.com>
2023-09-08 13:48:06 -0700

    updates

--- nvim/.config/nvim/lua/plugins/treesj.lua
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -1,7 +1,7 @@
 return {
   "Wansmer/treesj",
   keys = {
-    { "<C-j>", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
+    { "<leader>j", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
   },
   opts = { use_default_keymaps = false, max_join_length = 150 },
 }
