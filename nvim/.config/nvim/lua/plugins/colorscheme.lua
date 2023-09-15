ffe8c4a47783722b722725d5152f5dcd3283a439
A. Fox <anthonyfox1988@gmail.com>
2023-09-15 14:19:59 -0700

    delete arduino, add colors

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,6 @@
 return {
   {
     "catppuccin/nvim",
-    lazy = true,
     name = "catppuccin",
     opts = {
       color_overrides = {
@@ -10,6 +9,65 @@ return {
         },
       },
     },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = { style = "night" },
+  },
+  {
+    -- kanagawa
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    lazy = false,
+    opts = {
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = {
+          wave = {},
+          lotus = {},
+          dragon = {},
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+      },
+    },
+  },
+  {
+    "rose-pine/neovim",
+    lazy = false,
+    name = "rose-pine",
+    opts = {},
+  },
+  {
+    "mcchrish/zenbones.nvim",
+    dependencies = { "rktjmp/lush.nvim" },
+  },
+  { "Mofiqul/vscode.nvim" },
+  {
+    "metalelf0/jellybeans-nvim",
+    dependencies = { "rktjmp/lush.nvim" },
   },
   {
     "LazyVim/LazyVim",
