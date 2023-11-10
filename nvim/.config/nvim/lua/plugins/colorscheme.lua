d0c0574c3e2bedbae89c4a9396eb10788b020aaf
A. Fox <anthonyfox1988@gmail.com>
2023-11-10 15:13:45 -0800

    colorscheme updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,9 +5,9 @@ return {
     lazy = false,
     opts = {
       color_overrides = {
-        mocha = {
-          base = "#11111b",
-        },
+        -- mocha = {
+        --   base = "#11111b",
+        -- },
       },
     },
   },
@@ -15,49 +15,6 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     opts = { style = "night" },
-  },
-  {
-    -- kanagawa
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    lazy = false,
-    opts = {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = {
-          wave = {},
-          lotus = {},
-          dragon = {},
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-      -- luacheck: ignore
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
-      background = { -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
-      },
-    },
-  },
-  {
-    "oxfist/night-owl.nvim",
   },
   {
     "LazyVim/LazyVim",
