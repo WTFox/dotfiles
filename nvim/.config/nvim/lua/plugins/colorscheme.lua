f1a365bb906500726740fc16f502f0db5697f7ad
A. Fox <anthonyfox1988@gmail.com>
2024-01-21 03:41:03 +0000

    colorscheme && updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,12 @@ return {
     name = "catppuccin",
     lazy = false,
     opts = {
+      transparent_background = true,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
       background = {
         light = "latte",
         dark = "mocha",
@@ -19,34 +25,6 @@ return {
           base = "#11111b",
         },
         -- https://github.com/catppuccin/nvim/discussions/323#discussioncomment-6410765
-        -- mocha = {
-        --   rosewater = "#ffc9c9",
-        --   flamingo = "#ff9f9a",
-        --   pink = "#ffa9c9",
-        --   mauve = "#df95cf",
-        --   lavender = "#a990c9",
-        --   red = "#ff6960",
-        --   maroon = "#f98080",
-        --   peach = "#f9905f",
-        --   yellow = "#f9bd69",
-        --   green = "#b0d080",
-        --   teal = "#a0dfa0",
-        --   sky = "#a0d0c0",
-        --   sapphire = "#95b9d0",
-        --   blue = "#89a0e0",
-        --   text = "#e0d0b0",
-        --   subtext1 = "#d5c4a1",
-        --   subtext0 = "#bdae93",
-        --   overlay2 = "#928374",
-        --   overlay1 = "#7c6f64",
-        --   overlay0 = "#665c54",
-        --   surface2 = "#504844",
-        --   surface1 = "#3a3634",
-        --   surface0 = "#252525",
-        --   base = "#151515",
-        --   mantle = "#0e0e0e",
-        --   crust = "#080808",
-        -- },
         latte = {
           rosewater = "#c14a4a",
           flamingo = "#c14a4a",
@@ -75,60 +53,6 @@ return {
           mantle = "#f0ebce",
           crust = "#e8e3c8",
         },
-      },
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    opts = { style = "night" },
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = false, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = false },
-      statementStyle = { bold = false },
-      typeStyle = {},
-      transparent = false, -- do not set background color
-      dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = {
-          wave = {},
-          lotus = {},
-          dragon = {},
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-          PmenuSbar = { bg = theme.ui.bg_m1 },
-          PmenuThumb = { bg = theme.ui.bg_p2 },
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-        }
-      end,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
-      background = { -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
       },
     },
   },
