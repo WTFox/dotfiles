f24119f0400de4dac20751d9be7b9bbbf588b4a6
A. Fox <anthonyfox1988@gmail.com>
2024-02-07 14:14:38 -0800

    updates

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -4,13 +4,13 @@ return {
     sections = {
       lualine_a = {
         function()
-          return "♥ " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+          return "󰣐 " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
         end,
       },
       lualine_z = {
         function()
+          -- use %R for 24hr time
           local value = os.date("%I:%M %p") -- e.g. 01:36 PM
-          -- local value = os.date("%R") -- e.g. 13:36
           return " " .. value
         end,
       },
