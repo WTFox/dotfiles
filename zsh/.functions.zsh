8cc445f89e9682c69860bb16d7f1d792d1a7a3c6
A. Fox <anthonyfox1988@gmail.com>
2024-02-09 14:59:04 -0800

    add more paths to check for nvm

--- zsh/.functions.zsh
+++ zsh/.functions.zsh
@@ -10,7 +10,11 @@ function load_nvm() {
   if type nvm > /dev/null 2>&1; then
       return
   fi
+  export NVM_DIR="$HOME/.nvm"
+
   [ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 }
 
 function ubuntu(){
