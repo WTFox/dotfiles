3e05997b1d4d8d7499f2511af7ba0f308962d39f
A. Fox <anthonyfox1988@gmail.com>
2024-02-14 08:54:00 -0800

    spaces

--- zsh/.aliases_mac.zsh
+++ zsh/.aliases_mac.zsh
@@ -1,6 +1,7 @@
 # == macOS-Specific Aliases ==
 
 # == System ==
+
 alias afk="open -a ScreenSaverEngine"
 alias flushdns="dscacheutil -flushcache && killall -HUP mDNSResponder"  # Flush Directory Service cache
 alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"  # Hide hidden files
@@ -11,6 +12,7 @@ alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && k
 alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm install npm -g; npm update -g; sudo gem update --system; sudo gem update; sudo gem cleanup'
 
 # == Fun ==
+
 alias scream="afplay -v 0.2 ~/audio/goat-scream.mp3"
 alias attenzione="afplay -v 0.2 ~/audio/attenzione.mp3"
 alias gti='(scream &); git'
