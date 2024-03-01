136322d60bed6ef02a1d02e85a23dce3abb8911f
A. Fox <anthonyfox1988@gmail.com>
2024-03-01 14:12:00 -0800

    updates

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -17,7 +17,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     if vim.o.background == "light" then
       vim.fn.system("kitty +kitten themes Kanagawa_light")
     elseif vim.o.background == "dark" then
-      vim.fn.system("kitty +kitten themes Kanagawa")
+      vim.fn.system("kitty +kitten themes Kanagawa_dragon")
     else
       vim.fn.system("kitty +kitten themes Kanagawa")
     end
