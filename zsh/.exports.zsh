fac0a2c8a946b18fc643ad246a689b9da16d0ed0
A. Fox <anthonyfox1988@gmail.com>
2024-03-01 21:44:36 -0800

    updates

--- zsh/.exports.zsh
+++ zsh/.exports.zsh
@@ -13,10 +13,10 @@ export VISUAL='nvim'
 export PATH="$HOME/bin:$PATH"
 
 # fzf options (catppuccin theme)
-# export FZF_DEFAULT_OPTS=" \
-# --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-# --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 # golang
 export GOPATH="$HOME/dev/go"
