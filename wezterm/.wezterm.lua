1408ae83b16667aa7006eca15d10f6a74dbacd74
A. Fox <anthonyfox1988@gmail.com>
2024-03-24 11:00:07 -0700

    slightly darker light theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -18,7 +18,7 @@ local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
 		return "catppuccin-mocha"
 	else
-		return "GruvboxLight"
+		return "Gruvbox (Gogh)"
 	end
 end
 
