ba6ac829c4ad8499eca132c98d5df54c6d922a92
A. Fox <anthonyfox1988@gmail.com>
2024-04-05 12:59:02 -0700

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -23,6 +23,9 @@ vim.api.nvim_create_autocmd("OptionSet", {
   callback = switch_colorscheme,
 })
 
+vim.g.tokyobones = { darkness = "stark" }
+vim.g.zenbones = { lightness = "bright" }
+
 return {
   {
     -- follows the system theme
@@ -133,78 +136,7 @@ return {
     },
   },
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          DashboardHeader = { fg = theme.ui.special, bold = true },
-          Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-          PmenuSbar = { bg = theme.ui.bg_m1 },
-          PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-          PmenuThumb = { bg = theme.ui.bg_p2 },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          FloatTitle = { bg = "none" },
-          -- Save an hlgroup with dark background and dimmed foreground
-          -- so that you can use it where your still want darker windows.
-          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-          -- Popular plugins that open floats will link to NormalFloat by default;
-          -- set their background accordingly if you wish to keep them dark and borderless
-          LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-          WinSeparator = { fg = theme.ui.fg_dim },
-        }
-      end,
-      theme = "wave", -- Load "wave" theme when 'background' option is not set
-      background = { -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
-      },
-    },
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-  },
-  {
-    "Mofiqul/vscode.nvim",
-    lazy = false,
-  },
-  {
     "mcchrish/zenbones.nvim",
-    lazy = false,
     dependencies = {
       "rktjmp/lush.nvim",
     },
