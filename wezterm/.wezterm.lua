74fe6b22a532fd5b5ca74163ba7f01d1fc236f44
A. Fox <anthonyfox1988@gmail.com>
2024-04-10 16:00:57 -0700

    updates

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -143,9 +143,9 @@ config.font = wezterm.font({
 		"cv11=1", -- y with different ascender construction (ss01)
 		-- "cv12=1", -- u with traditional construction (ss01)
 		"cv14=1", -- $ with broken bar
-		"cv15=1", -- alternative ampersand
+		"cv15=1", -- & alternative ampersand
 		"cv16=1", -- Q with bent tail
-		"cv17=1", -- f with curly ascender (ss02)
+		-- "cv17=1", -- f with curly ascender (ss02)
 		-- "cv18=1", -- 269 variant
 		-- "cv19=1", -- 8 old variant
 		-- "cv20=1", -- 5 old variant
@@ -310,3 +310,53 @@ if is_windows then
 end
 
 return config
+
+-- Below is an example implementation of a theme switcher
+-- local wezterm = require("wezterm")
+-- local act = wezterm.action
+--
+-- local M = {}
+--
+-- M.theme_switcher = function(window, pane)
+--   -- get builting color schemes
+--   local schemes = wezterm.get_builtin_color_schemes()
+--   local choices = {}
+--   local config_path = "/path/to/.wezterm.lua"
+--
+--   -- populate theme names in choices list
+--   for key, _ in pairs(schemes) do
+--     table.insert(choices, { label = tostring(key) })
+--   end
+--
+--   -- sort choices list
+--   table.sort(choices, function(c1, c2)
+--     return c1.label < c2.label
+--   end)
+--
+--   window:perform_action(
+--     act.InputSelector({
+--       title = "🎨 Pick a Theme!",
+--       choices = choices,
+--       fuzzy = true,
+--
+--       -- execute 'sed' shell command to replace the line
+--           -- responsible of colorscheme in my config
+--       action = wezterm.action_callback(function(inner_window, inner_pane, _, label)
+--         inner_window:perform_action(
+--           act.SpawnCommandInNewTab({
+--             args = {
+--               "sed",
+--               "-i",
+--               '/^Colorscheme/c\\Colorscheme = "' .. label .. '"',
+--               config_path,
+--             },
+--           }),
+--           inner_pane
+--           )
+--       end),
+--       }),
+--       pane
+--   )
+-- end
+--
+-- return M
