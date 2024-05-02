ba7691a9f36dcd99bc8b0141d025b903168770e6
A. Fox <anthonyfox1988@gmail.com>
2024-05-02 08:49:43 -0700

    updates

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,22 +1,6 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
-    "nvim-neotest/neotest-go",
-    "nvim-neotest/neotest-python",
-    "haydenmeade/neotest-jest",
-    "rouge8/neotest-rust",
+    { "nvim-neotest/nvim-nio", version = "1.9.0" },
   },
-  opts = function(_, opts)
-    opts.adapters = vim.list_extend(opts.adapters or {}, {
-      require("neotest-go")({
-        args = { "-tags=integration" },
-      }),
-      require("neotest-python")({}),
-      require("neotest-jest")({}),
-      require("neotest-rust")({
-        args = { "--no-capture" },
-        dap_adapter = "lldb",
-      }),
-    })
-  end,
 }
