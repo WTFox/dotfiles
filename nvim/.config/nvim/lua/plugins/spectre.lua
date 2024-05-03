046f874f52f77d215be16c6788e9162cd348f8aa
A. Fox <anthonyfox1988@gmail.com>
2024-05-03 09:58:38 -0700

    updates

---
+++ nvim/.config/nvim/lua/plugins/spectre.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-pack/nvim-spectre",
+  keys = {
+    {
+      "<leader>sr",
+      '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+      desc = "Replace in File (Spectre)",
+    },
+  },
+}
