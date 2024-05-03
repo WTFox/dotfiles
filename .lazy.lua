338a464ea29925cbbaea5bd2fe7b0b9042d11c87
A. Fox <anthonyfox1988@gmail.com>
2024-05-02 17:41:01 -0700

    updates

--- .lazy.lua
+++ .lazy.lua
@@ -1,7 +1,27 @@
 return {
+	-- disable auto theme switching
 	{
 		"f-person/auto-dark-mode.nvim",
 		enabled = false,
 	},
-	{ "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-frappe" } },
+	-- different colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "habamax",
+		},
+	},
+	-- show hidden files in neotree by default
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignore = false,
+					hide_hidden = false,
+				},
+			},
+		},
+	},
 }
