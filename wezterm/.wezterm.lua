c374989a067a695120784a0d3c17a2bcef851b36
A. Fox <anthonyfox1988@gmail.com>
2024-05-13 16:20:34 -0700

    change font features

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -94,7 +94,7 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 
 	if has_unseen_output then
 		return {
-			{ Foreground = { Color = "#fab387" } },
+			{ Foreground = { Color = "#ff9e64" } },
 			{ Text = title },
 		}
 	end
@@ -153,8 +153,8 @@ local jetbrains_styled = {
 		-- "ss01", -- All classic construction. JetBrains Mono but even more neutral. Performs better in big paragraph of text.
 		-- "ss02", -- All closed construction. Change the rhythm to a more lively one.
 		-- "ss19", -- Adds gaps in ≠ ≠= == === ligatures.
-		-- "ss20", -- Shift horizontal stroke in f to match x-height
-		-- "cv01", -- l with symmetrical lower stroke. (ss01)
+		"ss20", -- Shift horizontal stroke in f to match x-height
+		"cv01", -- l with symmetrical lower stroke. (ss01)
 		-- "cv02", -- t with curly tail (ss02)
 		"cv03", -- g with more complex construction
 		-- "cv04", -- j with curly descender
@@ -165,7 +165,7 @@ local jetbrains_styled = {
 		-- "cv09", -- f with additional horizontal stroke. (ss01)
 		-- "cv10", -- r with more open construction (ss01)
 		"cv11", -- y with different ascender construction (ss01)
-		"cv12", -- u with traditional construction (ss01)
+		-- "cv12", -- u with traditional construction (ss01)
 		"cv14", -- $ with broken bar
 		"cv15", -- & alternative ampersand
 		"cv16", -- Q with bent tail
