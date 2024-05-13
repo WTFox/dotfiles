4bebf058e9506cd87d8e5516d3bf0967db8500a7
A. Fox <anthonyfox1988@gmail.com>
2024-05-13 07:19:28 -0700

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ if Utils.is_wsl() or Utils.wants_transparent_background() then
   transparent_background = true
 end
 
-local dark_theme = "catppuccin-macchiato"
+local dark_theme = "catppuccin-mocha"
 local light_theme = "zenbones"
 
 local function switch_colorscheme()
