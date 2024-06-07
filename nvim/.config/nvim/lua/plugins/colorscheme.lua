515343e607712d2068d2c707b284f71962f8e331
A. Fox <anthonyfox1988@gmail.com>
2024-06-07 19:23:50 +0000

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -17,7 +17,7 @@ return {
   {
     -- follows the system theme
     "f-person/auto-dark-mode.nvim",
-    config = {
+    opts = {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
