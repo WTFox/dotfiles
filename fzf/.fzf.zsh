18098782652e0dcc5a989ef909b0ceaed9ceea0e
A. Fox <anthonyfox1988@gmail.com>
2024-06-09 01:42:19 +0000

    add fzf colors and remove macos binary

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -11,3 +11,10 @@ source "${HOME}/.fzf/shell/completion.zsh"
 # Key bindings
 # ------------
 source "${HOME}/.fzf/shell/key-bindings.zsh"
+
+# Colors
+# Catppuccin
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#0e0e13,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
