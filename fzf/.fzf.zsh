25df2d41394800797b02c454c30651db033b3dcf
A. Fox <anthonyfox1988@gmail.com>
2024-06-24 14:47:40 -0700

    updates

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -14,10 +14,10 @@ source "${HOME}/.fzf/shell/key-bindings.zsh"
 
 # Colors
 # Catppuccin
-# export FZF_DEFAULT_OPTS=" \
-# --color=bg+:#313244,bg:#0e0e13,spinner:#f5e0dc,hl:#f38ba8 \
-# --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-# --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#0e0e13,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
 # Kanagawa Dragon
 # export FZF_DEFAULT_OPTS=" \
