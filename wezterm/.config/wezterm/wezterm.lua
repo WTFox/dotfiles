ee86575698b04fdcfa1006d6f55948092601cf01
A. Fox <anthonyfox1988@gmail.com>
2024-08-13 15:16:06 -0700

    remove padding

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -114,8 +114,8 @@ config.tab_bar_at_bottom = true
 config.default_cursor_style = "BlinkingBlock"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = 2,
-	right = 2,
+	left = 0,
+	right = 0,
 	top = 0,
 	bottom = 0,
 }
