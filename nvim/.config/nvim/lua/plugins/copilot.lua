ab207ecdd3f316a48d747df31831d0215f22ecf2
A. Fox <anthonyfox1988@gmail.com>
2024-09-04 15:20:04 -0700

    updates

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,23 +1,31 @@
 return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    panel = {
-      enabled = true,
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "gpt-4o",
     },
-    filetypes = {
-      markdown = true,
-      help = true,
-      text = false,
-      zsh = false,
-      sh = function()
-        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-          return false
-        end
-        if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.envrc.*") then
-          return false
-        end
-        return true
-      end,
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      panel = {
+        enabled = true,
+      },
+      filetypes = {
+        markdown = true,
+        help = true,
+        text = false,
+        zsh = false,
+        sh = function()
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+            return false
+          end
+          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.envrc.*") then
+            return false
+          end
+          return true
+        end,
+      },
     },
   },
 }
