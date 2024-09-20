6bc058e2b815d85bee761e37373a48b0ee846647
A. Fox <anthonyfox1988@gmail.com>
2024-09-20 08:50:06 -0700

    updates

--- wezterm/.config/wezterm/appearance.lua
+++ wezterm/.config/wezterm/appearance.lua
@@ -60,7 +60,7 @@ return {
 	use_fancy_tab_bar = false,
 	tab_bar_at_bottom = true,
 	default_cursor_style = "BlinkingBlock",
-	hide_tab_bar_if_only_one_tab = true,
+	hide_tab_bar_if_only_one_tab = false,
 	window_padding = {
 		left = 0,
 		right = 0,
