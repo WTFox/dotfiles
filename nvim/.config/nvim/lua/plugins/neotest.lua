5b58e2147b798ca7b9a86e18b917224b898d518b
A. Fox <anthonyfox1988@gmail.com>
2024-10-01 15:45:23 -0700

    add proper neotest config

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,9 +1,42 @@
 return {
   {
     "nvim-neotest/neotest",
-  },
-  {
-    "haydenmeade/neotest-jest",
-    ft = "javascript",
+    dependencies = {
+      "nvim-neotest/neotest-jest",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-jest")({
+            --   command = "~/path/to/jest",
+            --   args = {
+            --     "--config=jest.config.js",
+            --   },
+            --   working_directory = "~/path/to/project",
+          }),
+          require("neotest-python")({
+            -- python = "/path/to/python",
+            -- runner = "pytest",
+            -- args = {
+            --   "--pytest-arg-1",
+            --   "--pytest-arg-2",
+            --   "--pytest-arg-3",
+            -- },
+          }),
+        },
+        quickfix = {
+          enabled = false,
+        },
+        output = {
+          enabled = true,
+          open_on_run = true,
+        },
+        output_panel = {
+          enabled = true,
+          open = "botright split | resize 15",
+        },
+      })
+    end,
   },
 }
