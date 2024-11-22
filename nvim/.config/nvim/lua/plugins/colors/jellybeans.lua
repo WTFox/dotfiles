f3257a52027a65fe07e5b3018fcb5d43bde74f03
A. Fox <anthonyfox1988@gmail.com>
2024-11-21 16:11:59 -0800

    updates

--- nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
+++ nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
@@ -1,3 +1,30 @@
 return {
   "wtfox/jellybeans.nvim",
+  opts = {
+    on_highlights = function(hl, c)
+      -- hl.DiagnosticInfo = {
+      --   fg = c.perano,
+      -- }
+      -- hl.DiagnosticUnderlineInfo = {
+      --   fg = c.perano,
+      --   undercurl = true,
+      -- }
+
+      -- hl.DiagnosticWarn = {
+      --   fg = c.brandy,
+      -- }
+      -- hl.DiagnosticUnderlineWarn = {
+      --   fg = c.brandy,
+      --   undercurl = true,
+      -- }
+
+      -- hl.DiagnosticError = {
+      --   fg = "#ff0000",
+      -- }
+      -- hl.DiagnosticUnderlineError = {
+      --   fg = "#ff0000",
+      --   undercurl = true,
+      -- }
+    end,
+  },
 }
