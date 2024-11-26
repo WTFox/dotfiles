5f47ae37635b8aac379d4ff3b6462cd7a97fee72
A. Fox <anthonyfox1988@gmail.com>
2024-11-26 15:45:12 -0800

    updates

--- nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
+++ nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
@@ -1,34 +1,6 @@
 return {
   "wtfox/jellybeans.nvim",
   opts = {
-    on_highlights = function(hl, c)
-      hl.NormalFloat = {
-        bg = c.grey_one,
-      }
-
-      -- hl.DiagnosticInfo = {
-      --   fg = c.perano,
-      -- }
-      -- hl.DiagnosticUnderlineInfo = {
-      --   fg = c.perano,
-      --   undercurl = true,
-      -- }
-
-      -- hl.DiagnosticWarn = {
-      --   fg = c.brandy,
-      -- }
-      -- hl.DiagnosticUnderlineWarn = {
-      --   fg = c.brandy,
-      --   undercurl = true,
-      -- }
-
-      -- hl.DiagnosticError = {
-      --   fg = "#ff0000",
-      -- }
-      -- hl.DiagnosticUnderlineError = {
-      --   fg = "#ff0000",
-      --   undercurl = true,
-      -- }
-    end,
+    on_highlights = function(hl, c) end,
   },
 }
