a07e191489e5a7ac672031bbe2ddd850b1413b85
A. Fox <anthonyfox1988@gmail.com>
2024-12-31 08:22:56 -0800

    feat(nvim): add debug helpers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,9 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+_G.dd = function(...)
+  Snacks.debug.inspect(...)
+end
+_G.bt = function()
+  Snacks.debug.backtrace()
+end
+vim.print = _G.dd
+
 require("config.lazy")
