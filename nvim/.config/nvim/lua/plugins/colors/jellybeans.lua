486aa6f338185703dba13c71b15b2e73a2008289
A. Fox <anthonyfox1988@gmail.com>
2025-01-02 08:45:55 -0800

    updates

--- nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
+++ nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
@@ -1,6 +1,15 @@
 return {
   "wtfox/jellybeans.nvim",
+  -- dir = "~/dev/jellybeans.nvim",
   opts = {
-    on_highlights = function(hl, c) end,
+    -- on_colors = function(c)
+    --   local light_bg = "#ffffff"
+    --   local dark_bg = "#000000"
+    --   c.background = vim.o.background == "light" and light_bg or dark_bg
+    -- end,
+    --
+    -- on_highlights = function(hl, c)
+    --   hl.Constant = { fg = "#00ff00", bold = true }
+    -- end,
   },
 }
