d9d14f95b8533c9587ceb1d1ea2856f7970469d9
A. Fox <anthonyfox1988@gmail.com>
2025-01-05 16:22:40 -0800

    updates

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -59,5 +59,20 @@ return {
         { section = "startup" },
       },
     },
+    zen = {
+      win = {
+        enter = true,
+        fixbuf = false,
+        minimal = false,
+        width = 150,
+        height = 0,
+        backdrop = { transparent = true, blend = 10 },
+        keys = { q = false },
+        zindex = 40,
+        wo = {
+          winhighlight = "NormalFloat:Normal",
+        },
+      },
+    },
   },
 }
