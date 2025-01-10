da7c805c5dc4bd703bdedfc544f6d02de7057208
A. Fox <anthonyfox1988@gmail.com>
2025-01-10 12:39:04 -0800

    feat(lazy): more plugins

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,17 @@
+require("starship"):setup()
+
+require("bookmarks"):setup({
+	last_directory = { enable = false, persist = false },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	notify = {
+		enable = false,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
