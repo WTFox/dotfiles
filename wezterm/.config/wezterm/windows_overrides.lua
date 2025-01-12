a1e4b7c5df15d92bf3a5edc19e43be48dd2cf081
A. Fox <anthonyfox1988@gmail.com>
2025-01-11 18:40:59 -0800

    chore(lazy): updates

--- wezterm/.config/wezterm/windows_overrides.lua
+++ wezterm/.config/wezterm/windows_overrides.lua
@@ -3,7 +3,9 @@ local fonts = require("lib.fonts")
 
 return {
 	default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "~" },
-	font_size = 15,
+	font_size = 16,
+	font = wezterm.font(fonts.cascadia_code),
+	line_height = 1.0,
 	wsl_domains = {
 		{
 			name = "WSL:Ubuntu",
