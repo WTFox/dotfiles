aa8921981b8b936b46fa9a246133f3513e2350dc
A. Fox <anthonyfox1988@gmail.com>
2025-01-17 19:01:27 -0800

    feat(zsh): add useful wsl aliases

--- zsh/.aliases_linux.zsh
+++ zsh/.aliases_linux.zsh
@@ -1,6 +1,8 @@
-# == Linux-Specific Aliases ==
+# == Linux-Specific (WSL) Aliases ==
 
 # == System ==
 alias localip="ipconfig getifaddr en0"
 alias nvim="~/bin/nvim-linux64/bin/nvim"
 alias exp="/mnt/c/Windows/explorer.exe"
+alias pbcopy="xclip -selection clipboard"
+alias pbpaste="xsel --clipboard"
