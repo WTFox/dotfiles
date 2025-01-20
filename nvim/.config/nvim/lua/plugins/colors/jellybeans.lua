05f3639b88275c1142a0220bacc2bfd64c1a15cf
A. Fox <anthonyfox1988@gmail.com>
2025-01-20 13:58:01 -0800

    feat(nvim): add custom snacks picker highlights

--- nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
+++ nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
@@ -7,8 +7,47 @@ return {
     -- end,
 
     on_highlights = function(hl, c)
+      -- change namespace colors to blue for golang
       hl["@lsp.type.namespace.go"] = {
         fg = c.morning_glory,
+      }
+
+      -- flat ui for snacks picker
+      local prompt = c.mine_shaft
+      hl.SnacksPickerBorder = {
+        fg = c.background,
+        bg = c.background,
+      }
+      hl.SnacksPickerInput = {
+        fg = c.foreground,
+        bg = prompt,
+      }
+      hl.SnacksPickerInputBorder = {
+        fg = prompt,
+        bg = prompt,
+      }
+      hl.SnacksPickerBoxBorder = {
+        fg = prompt,
+        bg = prompt,
+      }
+      hl.SnacksPickerBoxTitle = {
+        fg = prompt,
+        bg = c.koromiko,
+      }
+      hl.SnacksPickerTitle = {
+        fg = c.foreground,
+        bg = prompt,
+      }
+      hl.SnacksPickerList = {
+        bg = prompt,
+      }
+      hl.SnacksPickerPrompt = {
+        fg = c.koromiko,
+        bg = prompt,
+      }
+      hl.SnacksPickerPreviewTitle = {
+        fg = c.background,
+        bg = c.biloba_flower,
       }
     end,
   },
