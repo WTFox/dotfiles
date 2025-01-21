843b5c95089aeb16e0ba4a46ad0a8cfd072569f1
A. Fox <anthonyfox1988@gmail.com>
2025-01-20 19:27:57 -0800

    feat(nvim): update dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -55,31 +55,8 @@ return {
       },
     },
     dashboard = {
-      preset = {
-        header = [[
-
-    ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
-    ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
-    ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
-    ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
-    ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
-    ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
-      },
       sections = {
         { section = "header" },
-        {
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "git status --short --branch",
-          height = 4,
-          padding = 1,
-          ttl = 5 * 60,
-          indent = 2,
-        },
         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { section = "startup" },
