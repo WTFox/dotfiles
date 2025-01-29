a99ef05b7de422c6bf432659829e64494045156e
A. Fox <anthonyfox1988@gmail.com>
2025-01-29 12:44:26 -0800

    feat(nvim): cwd=true for Snacks.dashboard.recent_files

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -58,7 +58,14 @@ return {
       sections = {
         { section = "header" },
         { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
-        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        {
+          icon = " ",
+          title = "Recent Files",
+          section = "recent_files",
+          cwd = true,
+          indent = 2,
+          padding = 1,
+        },
         { section = "startup" },
       },
     },
