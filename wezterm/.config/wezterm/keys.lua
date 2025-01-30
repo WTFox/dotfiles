e68dcb8f1889ab59091f1f40a59ea46d3fffd3c9
A. Fox <anthonyfox1988@gmail.com>
2025-01-29 17:54:05 -0800

    feat(wezterm.keys): utilize leader

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -8,155 +8,70 @@ end
 
 local act = wezterm.action
 local key_mod_panes = "CTRL|SHIFT"
+local NONE = "NONE"
+local mods = {
+	l = "LEADER",
+	c = "CTRL",
+	a = "ALT",
+	s = "SHIFT",
+	S = "SUPER",
+}
+
 return {
+	leader = { key = "b", mods = mods.c },
 	key_tables = {
 		search_mode = {
-			{ key = "c", mods = "CTRL", action = act.CopyMode("Close") },
-			{ key = "Enter", mods = "NONE", action = act.CopyMode("PriorMatch") },
-			{ key = "Escape", mods = "NONE", action = act.CopyMode("Close") },
-			{ key = "n", mods = "CTRL", action = act.CopyMode("NextMatch") },
-			{ key = "p", mods = "CTRL", action = act.CopyMode("PriorMatch") },
-			{ key = "r", mods = "CTRL", action = act.CopyMode("CycleMatchType") },
-			{ key = "u", mods = "CTRL", action = act.CopyMode("ClearPattern") },
-			{
-				key = "PageUp",
-				mods = "NONE",
-				action = act.CopyMode("PriorMatchPage"),
-			},
-			{
-				key = "PageDown",
-				mods = "NONE",
-				action = act.CopyMode("NextMatchPage"),
-			},
-			{ key = "UpArrow", mods = "NONE", action = act.CopyMode("PriorMatch") },
-			{ key = "DownArrow", mods = "NONE", action = act.CopyMode("NextMatch") },
+			{ key = "c", mods = mods.c, action = act.CopyMode("Close") },
+			{ key = "Enter", mods = NONE, action = act.CopyMode("PriorMatch") },
+			{ key = "Escape", mods = NONE, action = act.CopyMode("Close") },
+			{ key = "n", mods = mods.c, action = act.CopyMode("NextMatch") },
+			{ key = "p", mods = mods.c, action = act.CopyMode("PriorMatch") },
+			{ key = "r", mods = mods.c, action = act.CopyMode("CycleMatchType") },
+			{ key = "u", mods = mods.c, action = act.CopyMode("ClearPattern") },
+			{ key = "UpArrow", mods = NONE, action = act.CopyMode("PriorMatch") },
+			{ key = "DownArrow", mods = NONE, action = act.CopyMode("NextMatch") },
 		},
 	},
 	keys = {
-		{ key = "d", mods = "SUPER|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-		{ key = "d", mods = "SUPER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-		{ key = "p", mods = "SUPER|SHIFT", action = act.ActivateCommandPalette },
+		{ key = ".", mods = mods.l, action = act.ActivateCommandPalette },
+		{ key = "f", mods = mods.l, action = act.ToggleFullScreen },
+		{ key = "n", mods = mods.l, action = act({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "|", mods = mods.l, action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+		{ key = "-", mods = mods.l, action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+		{ key = "h", mods = mods.l, action = act({ ActivatePaneDirection = "Left" }) },
+		{ key = "j", mods = mods.l, action = act({ ActivatePaneDirection = "Down" }) },
+		{ key = "k", mods = mods.l, action = act({ ActivatePaneDirection = "Up" }) },
+		{ key = "l", mods = mods.l, action = act({ ActivatePaneDirection = "Right" }) },
+		{ key = "H", mods = mods.l, action = act({ ActivateTabRelative = -1 }) },
+		{ key = "L", mods = mods.l, action = act({ ActivateTabRelative = 1 }) },
+		{ key = "{", mods = key_mod_panes, action = act({ ActivateTabRelative = -1 }) },
+		{ key = "}", mods = key_mod_panes, action = act({ ActivateTabRelative = 1 }) },
+		{ key = "x", mods = mods.l, action = act({ CloseCurrentPane = { confirm = true } }) },
+		{ key = "s", mods = mods.l, action = act.PaneSelect({ mode = "SwapWithActiveKeepFocus" }) },
+		{ key = "z", mods = mods.l, action = act.TogglePaneZoomState },
+		{ key = "LeftArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Left", 5 }) },
+		{ key = "RightArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Right", 5 }) },
+		{ key = "DownArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Down", 5 }) },
+		{ key = "UpArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Up", 5 }) },
+		{ key = "UpArrow", mods = mods.a, action = act.ScrollByLine(-1) },
+		{ key = "DownArrow", mods = mods.a, action = act.ScrollByLine(1) },
+		{ key = "[", mods = mods.l, action = act.ActivateCopyMode },
+		-- open url quick select
 		{
-			key = "Enter",
-			mods = "ALT",
-			action = act.DisableDefaultAssignment,
-		},
-		{
-			key = "v",
-			mods = key_mod_panes,
-			action = act({ PasteFrom = "Clipboard" }),
-		},
-		{
-			key = "c",
-			mods = key_mod_panes,
-			action = act({ CopyTo = "Clipboard" }),
-		},
-		{
-			key = "|",
-			mods = key_mod_panes,
-			action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
-		},
-		{
-			key = "_",
-			mods = key_mod_panes,
-			action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }),
-		},
-		{
-			key = "h",
-			mods = key_mod_panes,
-			action = act({ ActivatePaneDirection = "Left" }),
-		},
-		{
-			key = "j",
-			mods = key_mod_panes,
-			action = act({ ActivatePaneDirection = "Down" }),
-		},
-		{
-			key = "k",
-			mods = key_mod_panes,
-			action = act({ ActivatePaneDirection = "Up" }),
-		},
-		{
-			key = "l",
-			mods = key_mod_panes,
-			action = act({ ActivatePaneDirection = "Right" }),
-		},
-		{
-			key = "h",
-			mods = "SUPER",
-			action = act({ ActivatePaneDirection = "Left" }),
-		},
-		{
-			key = "j",
-			mods = "SUPER",
-			action = act({ ActivatePaneDirection = "Down" }),
-		},
-		{
-			key = "k",
-			mods = "SUPER",
-			action = act({ ActivatePaneDirection = "Up" }),
-		},
-		{
-			key = "l",
-			mods = "SUPER",
-			action = act({ ActivatePaneDirection = "Right" }),
-		},
-		{
-			key = "{",
-			mods = key_mod_panes,
-			action = act({ ActivateTabRelative = -1 }),
-		},
-		{
-			key = "}",
-			mods = key_mod_panes,
-			action = act({ ActivateTabRelative = 1 }),
-		},
-		{
-			key = "f",
-			mods = key_mod_panes,
-			action = act.ToggleFullScreen,
-		},
-		{
-			key = "Backspace",
-			mods = key_mod_panes,
-			action = act({ CloseCurrentPane = { confirm = true } }),
-		},
-		{
-			key = "n",
-			mods = key_mod_panes,
-			action = act({ SpawnTab = "CurrentPaneDomain" }),
-		},
-		{ key = "UpArrow", mods = "SHIFT", action = act.ScrollByLine(-1) },
-		{ key = "DownArrow", mods = "SHIFT", action = act.ScrollByLine(1) },
-		{ key = "k", mods = key_mod_panes, action = act.ScrollByLine(-1) },
-		{ key = "j", mods = key_mod_panes, action = act.ScrollByLine(1) },
-		{
-			key = "p",
-			mods = key_mod_panes,
-			action = act.ShowLauncherArgs({ flags = "FUZZY|TABS|WORKSPACES" }),
-		},
-		{
-			key = ".",
-			mods = "CTRL",
-			action = act.ActivateCommandPalette,
-		},
-		{
-			key = "\\",
-			mods = "CTRL",
-			action = act.PaneSelect({
-				mode = "SwapWithActiveKeepFocus",
+			key = "i",
+			mods = mods.l,
+			action = wezterm.action({
+				QuickSelectArgs = {
+					patterns = quick_select_patterns,
+					action = wezterm.action_callback(function(window, pane)
+						local url = window:get_selection_text_for_pane(pane)
+						wezterm.log_info("opening: " .. url)
+						wezterm.open_with(url)
+					end),
+				},
 			}),
 		},
-		{
-			key = "Enter",
-			mods = key_mod_panes,
-			action = act.TogglePaneZoomState,
-		},
-		{
-			key = ".",
-			mods = key_mod_panes,
-			action = act.ActivateCopyMode,
-		},
+		-- wezterm.nvim support
 		{
 			key = ";",
 			mods = "CTRL",
@@ -179,20 +94,6 @@ return {
 					tab:set_zoomed(true)
 				end
 			end),
-		},
-		{
-			key = "P",
-			mods = key_mod_panes,
-			action = wezterm.action({
-				QuickSelectArgs = {
-					patterns = quick_select_patterns,
-					action = wezterm.action_callback(function(window, pane)
-						local url = window:get_selection_text_for_pane(pane)
-						wezterm.log_info("opening: " .. url)
-						wezterm.open_with(url)
-					end),
-				},
-			}),
 		},
 	},
 	mouse_bindings = {
