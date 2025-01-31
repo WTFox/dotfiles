3a15101ffda15a76d9120f453ccb7908b59e8e19
A. Fox <anthonyfox1988@gmail.com>
2025-01-30 17:16:31 -0800

    chore(lazy): updates

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -38,6 +38,7 @@ return {
 		{ key = "f", mods = mods.l, action = act.Search({ CaseInSensitiveString = "" }) },
 		{ key = "n", mods = mods.l, action = act({ SpawnTab = "CurrentPaneDomain" }) },
 		{ key = "|", mods = mods.l, action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+		{ key = "\\", mods = mods.l, action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) }, -- windows
 		{ key = "-", mods = mods.l, action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
 		{ key = "h", mods = mods.l, action = act({ ActivatePaneDirection = "Left" }) },
 		{ key = "j", mods = mods.l, action = act({ ActivatePaneDirection = "Down" }) },
