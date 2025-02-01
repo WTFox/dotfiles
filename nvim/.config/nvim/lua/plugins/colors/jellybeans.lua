eeeae39565b11b12cc8f7f24dd3aa969822451bd
A. Fox <anthonyfox1988@gmail.com>
2025-02-01 14:01:33 -0800

    feat(nvim): update jellybeans config

--- nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
+++ nvim/.config/nvim/lua/plugins/colors/jellybeans.lua
@@ -3,56 +3,20 @@ return {
   -- dir = "~/dev/jellybeans.nvim",
   opts = {
     italics = false,
+    -- style = "dark",
+    -- flat_ui = true,
+
     -- on_colors = function(c)
+    --   local light_bg = "#ffffff"
+    --   local dark_bg = "#000000"
+    --   c.background = vim.o.background == "light" and light_bg or dark_bg
     -- end,
 
     on_highlights = function(hl, c)
       -- change namespace colors to blue for golang
-      hl["@lsp.type.namespace.go"] = {
-        fg = c.morning_glory,
-      }
-
-      -- flat ui for snacks picker
-      local prompt = c.mine_shaft
-      hl.SnacksPickerBorder = {
-        fg = c.background,
-        bg = c.background,
-      }
-      hl.SnacksPickerInput = {
-        fg = c.foreground,
-        bg = prompt,
-      }
-      hl.SnacksPickerInputBorder = {
-        fg = prompt,
-        bg = prompt,
-      }
-      hl.SnacksPickerBoxBorder = {
-        fg = prompt,
-        bg = prompt,
-      }
-      hl.SnacksPickerBoxTitle = {
-        fg = prompt,
-        bg = c.koromiko,
-      }
-      hl.SnacksPickerTitle = {
-        fg = c.foreground,
-        bg = prompt,
-      }
-      hl.SnacksPickerList = {
-        bg = prompt,
-      }
-      hl.SnacksPickerPrompt = {
-        fg = c.koromiko,
-        bg = prompt,
-      }
-      hl.SnacksPickerPreviewTitle = {
-        fg = c.background,
-        bg = c.biloba_flower,
-      }
-      hl.SnacksPickerFlag = {
-        bg = c.koromiko,
-        fg = c.ripe_plum,
-      }
+      -- hl["@lsp.type.namespace.go"] = {
+      --   fg = c.morning_glory,
+      -- }
     end,
   },
 }
