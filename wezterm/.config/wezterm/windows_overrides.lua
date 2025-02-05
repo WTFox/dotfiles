aa8d9af00cbf7bcdbed42b55e42cd472459f790b
A. Fox <anthonyfox1988@gmail.com>
2025-02-04 16:36:39 -0800

    feat: make windows transparent

--- wezterm/.config/wezterm/windows_overrides.lua
+++ wezterm/.config/wezterm/windows_overrides.lua
@@ -3,6 +3,8 @@ local fonts = require("lib.fonts")
 
 return {
 	default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "~" },
+	window_background_opacity = 0,
+	win32_system_backdrop = "Tabbed",
 	wsl_domains = {
 		{
 			name = "WSL:Ubuntu",
