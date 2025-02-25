9a317c0c54697f12182a00aff6801d310991dd86
A. Fox <anthonyfox1988@gmail.com>
2025-02-25 09:14:49 -0800

    fix: eval brew for installation

--- _bootstrap/os/mac.sh
+++ _bootstrap/os/mac.sh
@@ -4,6 +4,7 @@ update_package_manager() {
   if ! command -v brew &>/dev/null; then
     echo "brew could not be found"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
   fi
 
   brew update
