b5acc0d333bbeb428d913027121adbdb9a62b5f8
A. Fox <anthonyfox1988@gmail.com>
2025-04-15 10:57:22 -0700

    feat: use harpoon2 instead of grapple

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,29 +1,25 @@
-if true then
-  return {}
-end
-
 return {
   "nvim-lspconfig",
   opts = {
     inlay_hints = {
       enabled = false,
     },
-    diagnostics = {
-      virtual_text = false,
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = "",
-          [vim.diagnostic.severity.WARN] = "",
-          [vim.diagnostic.severity.HINT] = "",
-          [vim.diagnostic.severity.INFO] = "",
-        },
-        numhl = {
-          [vim.diagnostic.severity.WARN] = "WarningMsg",
-          [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-          [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
-          [vim.diagnostic.severity.HINT] = "DiagnosticHint",
-        },
-      },
-    },
+    -- diagnostics = {
+    --   virtual_text = false,
+    --   signs = {
+    --     text = {
+    --       [vim.diagnostic.severity.ERROR] = "",
+    --       [vim.diagnostic.severity.WARN] = "",
+    --       [vim.diagnostic.severity.HINT] = "",
+    --       [vim.diagnostic.severity.INFO] = "",
+    --     },
+    --     numhl = {
+    --       [vim.diagnostic.severity.WARN] = "WarningMsg",
+    --       [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+    --       [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+    --       [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+    --     },
+    --   },
+    -- },
   },
 }
