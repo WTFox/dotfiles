71760e32d50c2d2bdbe2b3e151174df1d926945a
A. Fox <anthonyfox1988@gmail.com>
2025-05-08 18:08:36 -0700

    feat: update windows overrides for wezterm

--- wezterm/.config/wezterm/windows_overrides.lua
+++ wezterm/.config/wezterm/windows_overrides.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 local fonts = require("lib.fonts")
 
 return {
+	font_size = 15,
+	font = wezterm.font(fonts.jetbrains_styled),
 	default_prog = { "wsl.exe", "-d", "Ubuntu", "--cd", "~" },
-	-- window_background_opacity = 0.9,
-	-- win32_system_backdrop = "Tabbed",
 	wsl_domains = {
 		{
 			name = "WSL:Ubuntu",
@@ -12,4 +12,6 @@ return {
 			default_cwd = "~",
 		},
 	},
+	-- window_background_opacity = 0.9,
+	-- win32_system_backdrop = "Tabbed",
 }
