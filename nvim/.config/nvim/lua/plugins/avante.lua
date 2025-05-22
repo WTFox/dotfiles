a08bbb1810b04cd5896dbcdb92d348bde503975d
A. Fox <anthonyfox1988@gmail.com>
2025-05-22 15:09:25 -0700

    chore(lazy): updates

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -9,12 +9,6 @@ return {
     opts = {
       provider = "copilot",
       auto_suggestions_provider = "copilot",
-      -- claude = {
-      --   endpoint = "https://api.anthropic.com",
-      --   model = "claude-3-5-sonnet-20241022",
-      --   temperature = 0,
-      --   max_tokens = 4096,
-      -- },
     },
     build = "make",
     dependencies = {
