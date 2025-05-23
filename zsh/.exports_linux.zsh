395400d995f54cd35e49853defcb55eb22a4b36b
A. Fox <anthonyfox1988@gmail.com>
2025-05-23 14:14:03 -0700

    fix(linux): load nvm by default

--- zsh/.exports_linux.zsh
+++ zsh/.exports_linux.zsh
@@ -3,3 +3,13 @@
 export PATH=$PATH:/snap/bin
 export PATH="$HOME/bin/nvim-linux64/bin:$PATH"
 export PATH=$PATH:/usr/local/go/bin
+
+
+if type nvm > /dev/null 2>&1; then
+    return
+fi
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
