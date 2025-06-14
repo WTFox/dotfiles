047ee51ef05c27c314031ff90e3a6fcbed1f100e
A. Fox <anthonyfox1988@gmail.com>
2025-06-14 12:53:07 -0700

    chore(lazy): updates

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,28 +1,6 @@
-if true then
-  return {}
-end
 return {
   "saghen/blink.cmp",
-  dependencies = {
-    {
-      "giuxtaposition/blink-cmp-copilot",
-    },
-  },
   opts = {
-    sources = {
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-        },
-      },
-      default = {
-        "copilot",
-        "lsp",
-        "path",
-        "snippets",
-        "buffer",
-      },
-    },
+    signature = { enabled = true },
   },
 }
