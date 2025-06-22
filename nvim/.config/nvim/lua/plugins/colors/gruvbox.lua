981dc0e30fc242d416118bbf5698cf8fb4a1051b
A. Fox <anthonyfox1988@gmail.com>
2025-06-22 11:15:34 -0700

    chore(lazy): updates

--- nvim/.config/nvim/lua/plugins/colors/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/colors/gruvbox.lua
@@ -1,28 +1,48 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = true,
-  opts = {
-    terminal_colors = true, -- add neovim terminal colors
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = {
-      strings = true,
-      emphasis = true,
-      comments = true,
-      operators = false,
-      folds = true,
+  {
+    "sainnhe/gruvbox-material",
+    priority = 1000,
+    config = function()
+      local cmds = {
+        "let g:gruvbox_material_background = 'hard'",
+        "let g:gruvbox_material_transparent_background = 2",
+        "let g:gruvbox_material_diagnostic_line_highlight = 1",
+        "let g:gruvbox_material_diagnostic_virtual_text = 'colored'",
+        "let g:gruvbox_material_enable_bold = 1",
+        "let g:gruvbox_material_enable_italic = 1",
+      }
+
+      for _, cmd in ipairs(cmds) do
+        vim.cmd(cmd)
+      end
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = false, -- invert background for search, diffs, statuslines and errors
+      contrast = "hard", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
     },
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    inverse = false, -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = false,
-    transparent_mode = false,
   },
 }
