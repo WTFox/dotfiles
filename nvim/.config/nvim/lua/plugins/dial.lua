92330afe4212d3167b3504b55ee5bdb0a5666fd4
A. Fox <anthonyfox1988@gmail.com>
2025-09-18 17:35:06 -0700

    feat: updates and disable dialing alphabet

--- nvim/.config/nvim/lua/plugins/dial.lua
+++ nvim/.config/nvim/lua/plugins/dial.lua
@@ -1,14 +1,74 @@
 return {
     "monaqa/dial.nvim",
     keys = {
-        { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-        { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-        { "g<C-a>", function() return require("dial.map").inc_gnormal() end, expr = true, desc = "Increment" },
-        { "g<C-x>", function() return require("dial.map").dec_gnormal() end, expr = true, desc = "Decrement" },
-        { "<C-a>", function() return require("dial.map").inc_visual() end, mode = "v", expr = true, desc = "Increment" },
-        { "<C-x>", function() return require("dial.map").dec_visual() end, mode = "v", expr = true, desc = "Decrement" },
-        { "g<C-a>", function() return require("dial.map").inc_gvisual() end, mode = "v", expr = true, desc = "Increment" },
-        { "g<C-x>", function() return require("dial.map").dec_gvisual() end, mode = "v", expr = true, desc = "Decrement" },
+        {
+            "<C-a>",
+            function()
+                return require("dial.map").inc_normal()
+            end,
+            expr = true,
+            desc = "Increment",
+        },
+        {
+            "<C-x>",
+            function()
+                return require("dial.map").dec_normal()
+            end,
+            expr = true,
+            desc = "Decrement",
+        },
+        {
+            "g<C-a>",
+            function()
+                return require("dial.map").inc_gnormal()
+            end,
+            expr = true,
+            desc = "Increment",
+        },
+        {
+            "g<C-x>",
+            function()
+                return require("dial.map").dec_gnormal()
+            end,
+            expr = true,
+            desc = "Decrement",
+        },
+        {
+            "<C-a>",
+            function()
+                return require("dial.map").inc_visual()
+            end,
+            mode = "v",
+            expr = true,
+            desc = "Increment",
+        },
+        {
+            "<C-x>",
+            function()
+                return require("dial.map").dec_visual()
+            end,
+            mode = "v",
+            expr = true,
+            desc = "Decrement",
+        },
+        {
+            "g<C-a>",
+            function()
+                return require("dial.map").inc_gvisual()
+            end,
+            mode = "v",
+            expr = true,
+            desc = "Increment",
+        },
+        {
+            "g<C-x>",
+            function()
+                return require("dial.map").dec_gvisual()
+            end,
+            mode = "v",
+            expr = true,
+            desc = "Decrement",
+        },
     },
     config = function()
         local augend = require("dial.augend")
@@ -23,8 +83,8 @@ return {
                 augend.date.alias["%Y-%m-%d"],
                 augend.date.alias["%-m/%-d"],
                 augend.date.alias["%Y年%-m月%-d日"],
-                augend.constant.alias.alpha,
-                augend.constant.alias.Alpha,
+                -- augend.constant.alias.alpha,
+                -- augend.constant.alias.Alpha,
                 augend.constant.new({
                     elements = { "and", "or" },
                     word = true,
