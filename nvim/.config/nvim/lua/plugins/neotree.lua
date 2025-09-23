3f9ae27d18d9681d32c1ba0df448e4ff235015d5
A. Fox <anthonyfox1988@gmail.com>
2025-09-23 09:09:51 -0700

    fix: neotree keymaps

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -6,32 +6,47 @@ return {
     },
     cmd = "Neotree",
     keys = {
-        { "<leader>fe", function()
-            require("neo-tree.command").execute({
-                toggle = true,
-                reveal_file = vim.fn.expand("%:p"),
-            })
-        end, desc = "Explorer NeoTree (Reveal Current File)" },
-        { "<leader>fE", function()
-            require("neo-tree.command").execute({
-                toggle = true,
-                dir = vim.uv.cwd(),
-            })
-        end, desc = "Explorer NeoTree (cwd)" },
+        {
+            "<leader>fe",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                })
+            end,
+            desc = "Explorer NeoTree (Reveal Current File)",
+        },
+        {
+            "<leader>fE",
+            function()
+                require("neo-tree.command").execute({
+                    toggle = true,
+                    dir = vim.uv.cwd(),
+                })
+            end,
+            desc = "Explorer NeoTree (cwd)",
+        },
         { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
         { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-        { "<leader>ge", function()
-            require("neo-tree.command").execute({
-                source = "git_status",
-                toggle = true,
-            })
-        end, desc = "Git Explorer" },
-        { "<leader>be", function()
-            require("neo-tree.command").execute({
-                source = "buffers",
-                toggle = true,
-            })
-        end, desc = "Buffer Explorer" },
+        {
+            "<leader>ge",
+            function()
+                require("neo-tree.command").execute({
+                    source = "git_status",
+                    toggle = true,
+                })
+            end,
+            desc = "Git Explorer",
+        },
+        {
+            "<leader>be",
+            function()
+                require("neo-tree.command").execute({
+                    source = "buffers",
+                    toggle = true,
+                })
+            end,
+            desc = "Buffer Explorer",
+        },
     },
     deactivate = function()
         vim.cmd([[Neotree close]])
