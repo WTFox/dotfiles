fb45c4db79c26c8860be36c122d2fc0fc321e0eb
A. Fox <anthonyfox1988@gmail.com>
2025-10-02 10:43:36 -0700

    chore(lazy): updates

--- wezterm/.config/wezterm/windows_overrides.lua
+++ wezterm/.config/wezterm/windows_overrides.lua
@@ -4,8 +4,6 @@ local wezterm = require("wezterm")
 
 ---@type StrictConfig
 return {
-    foobar = 15,
-    line_height = 1,
     -- font = wezterm.font(fonts.jetbrains_styled),
     default_prog = { "wsl.exe", "~" },
     default_domain = "WSL:Ubuntu",
