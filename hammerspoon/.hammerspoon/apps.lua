09fdbb2440b2df16de7ed899e8dbdc764adf6447
A. Fox <anthonyfox1988@gmail.com>
2025-10-09 13:39:40 -0700

    feat: nope.. back to wezterm

--- hammerspoon/.hammerspoon/apps.lua
+++ hammerspoon/.hammerspoon/apps.lua
@@ -2,7 +2,7 @@ local utils = require("utils")
 
 local prefix = { "ctrl" }
 local mappings = {
-    { key = "`", app = "ghostty" },
+    { key = "`", app = "Wezterm" },
     { key = "1", app = "Firefox" },
 }
 
