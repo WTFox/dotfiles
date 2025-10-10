c40519c6e2b5836152c29b3fb7c84f1d3668c313
A. Fox <anthonyfox1988@gmail.com>
2025-10-10 08:41:54 -0700

    feat: lazy load nvm

--- zsh/.exports_linux.zsh
+++ zsh/.exports_linux.zsh
@@ -5,9 +5,3 @@ export PATH="$HOME/bin/nvim-linux64/bin:$PATH"
 export PATH=$PATH:/usr/local/go/bin
 
 
-if type nvm > /dev/null 2>&1; then
-    return
-fi
-export NVM_DIR="$HOME/.nvm"
-
-[ -s "$HOME/.nvm/nvm.sh" ] && \. "$HOME/.nvm/nvm.sh"
