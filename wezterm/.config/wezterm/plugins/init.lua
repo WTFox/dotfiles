268c809205db64482ebd9913f3433bd8c4546e67
A. Fox <anthonyfox1988@gmail.com>
2025-10-15 09:46:08 -0700

    feat(wezterm): add presentation.wez

--- wezterm/.config/wezterm/plugins/init.lua
+++ wezterm/.config/wezterm/plugins/init.lua
@@ -1,8 +1,9 @@
 local M = {}
 
 M.setup = function(config)
-	require("plugins.tabline").setup(config)
-	require("plugins.sessionizer").setup(config)
+    require("plugins.tabline").setup(config)
+    require("plugins.sessionizer").setup(config)
+    require("plugins.presentation").setup(config)
 end
 
 return M
---
+++ wezterm/.config/wezterm/plugins/presentation.lua
@@ -0,0 +1,21 @@
+local utils = require("lib.utils")
+local wezterm = require("wezterm") --[[@as Wezterm]]
+
+local M = {}
+
+M.setup = function(config)
+    local presentation = wezterm.plugin.require("https://gitlab.com/xarvex/presentation.wez")
+    presentation.apply_to_config(config, {
+        font_size_multiplier = 1.8, -- sets for both "presentation" and "presentation_full"
+        presentation = {
+            keybind = { key = "t", mods = "SHIFT|SUPER" }, -- setting a keybind
+        },
+        presentation_full = {
+            keybind = { key = "t", mods = "CTRL|SHIFT|SUPER" }, -- setting a keybind
+            font_weight = "Bold",
+            font_size_multiplier = 2.4, -- overwrites "font_size_multiplier" for "presentation_full"
+        },
+    })
+end
+
+return M
