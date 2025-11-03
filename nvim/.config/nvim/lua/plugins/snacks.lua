9936746dcb9084fc7c7b4685e70fb045e5a6d803
A. Fox <anthonyfox1988@gmail.com>
2025-11-03 07:18:59 -0800

    feat(nvim.snacks): add gh keybinds

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -740,6 +740,35 @@ return {
             end,
             desc = "Notification History",
         },
+        -- gh
+        {
+            "<leader>gi",
+            function()
+                Snacks.picker.gh_issue()
+            end,
+            desc = "GitHub Issues (open)",
+        },
+        {
+            "<leader>gI",
+            function()
+                Snacks.picker.gh_issue({ state = "all" })
+            end,
+            desc = "GitHub Issues (all)",
+        },
+        {
+            "<leader>gp",
+            function()
+                Snacks.picker.gh_pr()
+            end,
+            desc = "GitHub Pull Requests (open)",
+        },
+        {
+            "<leader>gP",
+            function()
+                Snacks.picker.gh_pr({ state = "all" })
+            end,
+            desc = "GitHub Pull Requests (all)",
+        },
     },
     init = function()
         vim.api.nvim_create_autocmd("User", {
