b7967237fe5a2c8237062d4c67fb597c4b9a802d
A. Fox <anthonyfox1988@gmail.com>
2025-11-07 08:32:33 -0800

    refactor: clean up comments

--- wezterm/.config/wezterm/appearance.lua
+++ wezterm/.config/wezterm/appearance.lua
@@ -3,38 +3,24 @@ local wezterm = require("wezterm") --[[@as Wezterm]]
 
 ---@type StrictConfig
 return {
-    -- font_size = 15.1,
-    -- line_height = 1.1,
-    font = wezterm.font_with_fallback({
-        fonts.berkeley_mono,
-        -- fonts.mononoki,
-        -- fonts.monolisa,
-        -- fonts.cascadia_code,
-        -- fonts.ioskeley_mono,
-        -- fonts.jetbrains,
-        -- fonts.jetbrains_styled,
-    }),
+    font = wezterm.font_with_fallback({ fonts.berkeley_mono }),
     adjust_window_size_when_changing_font_size = false,
     use_fancy_tab_bar = true,
     hide_tab_bar_if_only_one_tab = false,
     default_cursor_style = "BlinkingBlock",
-    cursor_blink_rate = 500, -- Optimize blink rate (ms)
+    cursor_blink_rate = 1000,
     initial_cols = 136,
     initial_rows = 35,
     bold_brightens_ansi_colors = true,
     window_decorations = "RESIZE|INTEGRATED_BUTTONS",
-    -- window_background_opacity = 0.9,
-    -- macos_window_background_blur = 95,
-
-    -- Remove window padding to eliminate empty space at bottom
     window_padding = {
         left = 0,
         right = 0,
         top = 0,
         bottom = 0,
     },
-
-    -- macOS fullscreen behavior
     native_macos_fullscreen_mode = false, -- Keep fast toggle
     macos_fullscreen_extend_behind_notch = true, -- Extend behind notch for cleaner look
+    -- window_background_opacity = 0.9,
+    -- macos_window_background_blur = 95,
 }
--- wezterm/.config/wezterm/behavior.lua
+++ wezterm/.config/wezterm/behavior.lua
@@ -11,18 +11,12 @@ local wezterm = require("wezterm") --[[@as Wezterm]]
 ---@type StrictConfig
 return {
     force_reverse_video_cursor = true,
-    -- Use xterm-256color for SSH compatibility (remote servers have this terminfo)
-    -- wezterm terminfo not available on most remote systems
     term = "xterm-256color",
     hyperlink_rules = wezterm.default_hyperlink_rules(),
     automatically_reload_config = false,
     enable_kitty_keyboard = true,
     enable_csi_u_key_encoding = true,
-
-    -- Performance optimizations
-    front_end = "WebGpu", -- Use Metal on macOS for best performance
-    -- animation_fps = 1, -- Reduce animation overhead (default 10)
-    -- max_fps = 120, -- Limit max frame rate
-    scrollback_lines = 10000, -- Reasonable scrollback (default 3500)
+    front_end = "WebGpu",
+    scrollback_lines = 10000,
     ssh_domains = wezterm.default_ssh_domains(),
 }
