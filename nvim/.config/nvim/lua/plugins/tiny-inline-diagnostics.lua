e292c30ede731a93916a0ae2b2a973577ce276b1
A. Fox <anthonyfox1988@gmail.com>
2025-11-11 10:47:04 -0800

    chore(lazy): updates

--- nvim/.config/nvim/lua/plugins/tiny-inline-diagnostics.lua
+++ nvim/.config/nvim/lua/plugins/tiny-inline-diagnostics.lua
@@ -2,13 +2,13 @@ return {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "VeryLazy",
     priority = 1000,
-    config = function()
-        require("tiny-inline-diagnostic").setup({
-            options = {
-                show_source = { enabled = true },
-                multilines = { enabled = true },
-                add_messages = { display_count = true },
-            },
-        })
-    end,
+    opts = {
+        options = {
+            show_all_diags_on_cursorline = false,
+            show_diags_only_under_cursor = true,
+            show_source = { enabled = true },
+            multilines = { enabled = true },
+            add_messages = { display_count = true },
+        },
+    },
 }
