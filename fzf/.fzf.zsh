bc017a578925917e8fa42044e7a28bb40386a8a5
A. Fox <anthonyfox1988@gmail.com>
2025-12-11 08:32:02 -0800

    feat(fzf): better colors

--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -14,8 +14,8 @@ source "${HOME}/.fzf/shell/key-bindings.zsh"
 
 # Jellybeans Muted colors
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#3c3b38,bg:#101010,spinner:#7db7cc,hl:#d9a45a \
---color=fg:#dad6c8,header:#d9a45a,info:#7a8aa6,pointer:#d8a16c \
---color=marker:#98b67c,fg+:#f9f5ec,prompt:#83adc3,hl+:#7a8aa6 \
---color=border:#3c3936 \
+--color=bg+:#1f1f1f,bg:#101010,spinner:#7db7cc,hl:#d9a45a \
+--color=fg:#e8e8d3,header:#cc88a3,info:#b0d0f0,pointer:#d8a16c \
+--color=marker:#99b67c,fg+:#e8e8d3,prompt:#83adc3,hl+:#e6a75a \
+--color=border:#101010 \
 --multi"
