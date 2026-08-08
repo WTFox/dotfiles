f0d9232d6c76f49b8087be4f2899b71a355f9600
A. Fox <anthonyfox1988@gmail.com>
2026-08-08 09:10:29 -0700

    feat: add clippy to rust lsp

--- nvim/.config/nvim/lsp/rust_analyzer.lua
+++ nvim/.config/nvim/lsp/rust_analyzer.lua
@@ -1,4 +1,54 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { vim.fn.expand("~/.cargo/bin/rust-analyzer") },
+    -- cmd = { vim.fn.expand("~/.cargo/bin/rust-analyzer") },
+    cmd = { "rust-analyzer" },
+    filetypes = { "rust" },
+    root_markers = { "Cargo.toml", "rust-project.json", ".git" },
+    settings = {
+        ["rust-analyzer"] = {
+            cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            -- Use clippy on save for better diagnostics
+            checkOnSave = true,
+            check = {
+                command = "clippy",
+                extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+                enable = true,
+                ignored = {
+                    ["async-trait"] = { "async_trait" },
+                    ["napi-derive"] = { "napi" },
+                    ["async-recursion"] = { "async_recursion" },
+                },
+            },
+            inlayHints = {
+                bindingModeHints = { enable = false },
+                chainingHints = { enable = true },
+                closingBraceHints = { enable = true, minLines = 25 },
+                closureReturnTypeHints = { enable = "never" },
+                lifetimeElisionHints = { enable = "never", useParameterNames = false },
+                maxLength = 25,
+                parameterHints = { enable = true },
+                reborrowHints = { enable = "never" },
+                renderColons = true,
+                typeHints = {
+                    enable = true,
+                    hideClosureInitialization = false,
+                    hideNamedConstructor = false,
+                },
+            },
+            diagnostics = {
+                enable = true,
+                experimental = {
+                    enable = true,
+                },
+            },
+        },
+    },
 }
